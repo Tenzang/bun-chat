427b0848920e12445f2ff5a44d4e61c7a1f51fdc
Loden <loden.gendun@students.mq.edu.au>
2023-01-30 13:05:31 +1100

    chore: removed test route

--- main.ts
+++ main.ts
@@ -6,14 +6,11 @@ const port = 3000;
 const router = new Router();
 
 router.static("./public");
+router.notFound(() => "404, not found");
 
 router.route("GET", "/", () => {
   return "index reached";
 });
-
-router.route("GET", "/ayyy", async () => "ayyy");
-
-router.notFound(() => "404, not found");
 
 export default {
   fetch: router.response,
