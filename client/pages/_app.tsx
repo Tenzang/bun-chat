63255fc3f0f80c58c5d54b82f777d00a7f2c9a80
Tenzang <loden.gendun@students.mq.edu.au>
2023-04-01 11:09:41 +1100

    chore: cleared out unnecessary code/files

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,20 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Bun Chat</title>
+				<meta
+					name="description"
+					content="A chat app built on the Bun Javascript runtime!"
+				/>
+				<link rel="icon" href="/favicon.png" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
