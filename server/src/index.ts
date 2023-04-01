8bd91da61e18a7f796f42929ccd698cce50ac965
Tenzang <loden.gendun@students.mq.edu.au>
2023-04-01 22:35:35 +1100

    feat: server sends to all open sockets on message

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,20 +3,28 @@ import { Message } from "./types";
 import { messages } from "./mockData";
 import { addMessage } from "./helpers";
 
+interface Sockets {
+	[key: string]: any;
+}
+
+const openSockets: Sockets = {};
+
 const app = new Elysia()
 	.use(ws())
 	.ws("/ws", {
 		message(ws, message) {
 			console.log(message);
 			addMessage(messages, message as Omit<Message, "id">);
-			ws.send(messages);
+			Object.values(openSockets).forEach((ws) => ws.send(messages));
 		},
 		open(ws) {
 			console.log("WEBSOCKET OPENED", ws.data.id);
 			ws.send(messages);
+			openSockets[ws.data.id] = ws;
 		},
 		close(ws) {
 			console.log("WEBSOCKET closed", ws.data.id);
+			delete openSockets[ws.data.id];
 		},
 	})
 	.get("/", () => "Hello Elysia")
