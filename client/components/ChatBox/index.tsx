01df9b6caf71f70153f7ac8b2972bc0f4b3cead1
Tenzang <loden.gendun@students.mq.edu.au>
2023-04-01 12:28:56 +1100

    feat: created ChatBox scaffold

---
+++ client/components/ChatBox/index.tsx
@@ -0,0 +1,8 @@
+export default () => {
+	return (
+		<div>
+			<div>messages will be displayed here...</div>
+			<input placeholder="type your message here..."></input>
+		</div>
+	);
+};
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
+import { ChatBox } from "../components";
 
 export default function Home({}) {
 	return (
 		<div className={styles.container}>
 			<main className={styles.main}>
 				<h1>Bun Chat</h1>
+				<ChatBox />
 			</main>
 		</div>
 	);
