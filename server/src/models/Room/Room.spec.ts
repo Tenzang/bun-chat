53a3fc922d7bc60e050380a87e35b117988f50ae
Tenzang <loden.gendun@students.mq.edu.au>
2023-04-03 11:38:49 +1000

    feat: Room .addMessage()

--- server/src/models/Room/Room.spec.ts
+++ server/src/models/Room/Room.spec.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from "bun:test";
 import Room from ".";
+import Message from "../Message";
+import { mockMessage } from "../Message/mocks";
 
 describe("Room", () => {
 	it("exists", () => {
@@ -10,5 +12,18 @@ describe("Room", () => {
 	it("contains messages", () => {
 		const room = new Room();
 		expect(room.messages).toBeDefined();
+	});
+
+	describe(".addMessage()", () => {
+		const { author, content } = mockMessage;
+
+		it("adds a message to rooms messages", () => {
+			const message = new Message(author, content);
+
+			const room = new Room();
+			room.addMessage(message);
+
+			expect(room.messages).toContain(message);
+		});
 	});
 });
--- server/src/models/Room/index.ts
+++ server/src/models/Room/index.ts
@@ -2,4 +2,8 @@ import Message from "../Message";
 
 export default class Room {
 	messages: Message[] = [];
+
+	addMessage(message: Message) {
+		this.messages.push(message);
+	}
 }
