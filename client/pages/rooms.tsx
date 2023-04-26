8ee40a5999b96a72ee327df1e67e600f21638193
Tenzang <loden.gendun@students.mq.edu.au>
2023-04-26 17:17:06 +1000

    client: feat: Room page

--- client/pages/rooms.tsx
+++ client/pages/rooms.tsx
@@ -1,3 +1,23 @@
+import { useEffect, useState } from "react";
+import API from "../utils/API";
+
+type Rooms = Awaited<ReturnType<typeof API.getRooms>>;
+
 export default () => {
-	return <div>room selector coming soon...</div>;
+	const [rooms, setRooms] = useState<Rooms>();
+
+	useEffect(() => {
+		API.getRooms().then((rooms) => {
+			console.log(rooms);
+			setRooms(rooms);
+		});
+	});
+
+	return (
+		<div>
+			{rooms
+				? rooms.map((room) => <button key={room.id}>{room.name}</button>)
+				: "loading..."}
+		</div>
+	);
 };
