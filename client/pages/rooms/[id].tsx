91c973c3bb25caf04f7cadb7da861542ea029407
Tenzang <loden.gendun@students.mq.edu.au>
2023-04-27 12:40:36 +1000

    client: refactor: remove unnecessary useEffect

--- client/pages/rooms/[id].tsx
+++ client/pages/rooms/[id].tsx
@@ -1,11 +1,8 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import ChatBox from "../../components/ChatBox";
 
 export default () => {
 	const { id } = useRouter().query;
-
-	useEffect(() => {});
 
 	return <ChatBox roomId={id as string} />;
 };
